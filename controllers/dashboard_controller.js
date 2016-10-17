10be26cff78e09f970eebfc643d7245c3bad8d3c
Ramiro Lopez <lopezdiaz.ramiro@gmail.com>
2016-10-16 23:17:39 -0700

    Added Error handling & Messaging for Login, Change Password, Sign up Pages. Added Back button to Change Password page. Changed Tim Styling for landing page & Buttons. Etc...

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -8,16 +8,16 @@ var session = require('client-sessions');
 //Manually adding one user into the database and then adding a project to that user
 
 module.exports = function(app) {
-	function requireLogin (req, res, next){
-		if(!req.user){
-			res.redirect('/login');
-		} else {
-			next();
-		}
-	};
+    function requireLogin(req, res, next) {
+        if (!req.user) {
+            res.redirect('/login');
+        } else {
+            next();
+        }
+    };
 
     router.get('/', requireLogin, function(req, res) {
-        res.render("dashboard/dashboard", {test: "Jonathan"});
+        res.render("dashboard/dashboard", { test: "Jonathan" });
     });
 
     //This allows you to route to any named view dynamically assuming that it exists
