229c5af064dc0fa78b8905efa4108a6098281980
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2016-10-17 21:42:45 -0700

    made changes to make projects page

--- controllers/portfolio_controller.js
+++ controllers/portfolio_controller.js
@@ -3,25 +3,35 @@ var router = express.Router();
 //var user = require('../models/user.js');
 var models = require('../models');
 var user = models.User;
-var project = models.Projects;
+var project = models.Project;
 var sequelizeConnection = models.sequelize;
 var session = require('client-sessions');
 
 //Manually adding one user into the database and then adding a project to that user
 
 module.exports = function(app) {
-    router.get('/samplePortfolio', function(req, res){
-        res.render("portfolio/sample", {layout:"sampleportfolio"});
+    router.get('/samplePortfolio', function(req, res) {
+        res.render("portfolio/sample", { layout: "sampleportfolio" });
     })
 
-    function renderPage(res, username, page){
+    function renderPage(res, username, page) {
         user.findOne({
             where: {
                 "username": username
             }
         }).then(function(data) {
             res.locals.user = data;
-            res.render(page, { layout: "sampleportfolio"});
+
+
+            project.findAll({
+                where: {
+                    UserId: data.id
+                }
+            }).then(function(projectData) {
+                
+                res.locals.projects = projectData;
+                res.render(page, { layout: "sampleportfolio" });
+            })
         });
     };
 
@@ -31,6 +41,7 @@ module.exports = function(app) {
 
     router.get('/:portfolio/portfolio', function(req, res) {
         renderPage(res, req.params.portfolio, "portfolio/portfolio");
+
     });
 
     router.get('/:portfolio/about', function(req, res) {
@@ -38,7 +49,7 @@ module.exports = function(app) {
     });
 
     router.get('/:portfolio/contact', function(req, res) {
-        renderPage(res, req.params.portfolio, "portfolio/contact");    
+        renderPage(res, req.params.portfolio, "portfolio/contact");
     });
 
 
