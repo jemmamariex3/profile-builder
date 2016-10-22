9c5d8280c31ff4aa10bc60854bb766bfae53bff1
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-10-22 09:00:10 -0700

    Added Jemma's Data

--- controllers/api/demodata_controller.js
+++ controllers/api/demodata_controller.js
@@ -175,6 +175,70 @@ module.exports = function(app) {
 
                     res.json("It worked");
                 })
+            user.create({
+                username: "jTiongson",
+                firstName: "Jemma",
+                lastName: "Tiongson",
+                email: "jtiongson@gmail.com",
+                password: "$2a$10$kUBvM0Czt0SSP9H1glH9lu49wwtvtfxLDeH0q93Z66Tbiirvxcnsq",
+                description: "I am currently a student at UCLA Coding Bootcamp training to become a Jr. Web Developer. I aim to intern at a small web-dev company to enrich my coding skills as I pursue my Computer Science B.S at Cal State University Northridge.",
+                linkGitHub: "https://github.com/jemmamariex3",
+                linkFacebook: "",
+                linkStack: "https://stackoverflow.com/users/7017647/jemmamariex3",
+                linkTwitter: "",
+                linkLinkedIn: "https://www.linkedin.com/in/jemma-tiongson-a6b027b0",
+                headline: "Jr. Web Developer"
+            })
+                .then(function (userData) {
+                    userId = userData.dataValues.id;
+                })
+                .then(function(userData) {
+                    console.log(userId);
+                    project.bulkCreate([
+                        {
+                            UserId: userId,
+                            name: "Burger App",
+                            linkLiveDemo: "",
+                            description: "The Burger app incorporates handlebars, MySQL and the MVC model skills that allows user to devour choices of burgers.",
+                            linkGitHub: "https://github.com/jemmamariex3/Week-15---Burger-2"
+                        },
+                        {
+                            UserId: userId,
+                            name: "MyPolity",
+                            linkLiveDemo: "",
+                            description: "MyPolity is a web app that gives users information about their federal, state and local government representatives.",
+                            linkGitHub: "https://github.com/jackemuk/MyPolity"
+                        }
+                    ]).then(function (projectData) {
+                        console.log("Projects added for " + userData.name)
+                    });
+                })
+                .then(function (userData) {
+                    service.bulkCreate([
+                        {
+                            UserId: userId,
+                            title: "HTML/Bootstrap",
+                            description: "I will create an html website to your specification " +
+                            "using bootstrap."
+                        },
+                        {
+                            UserId: userId,
+                            title: "Javascript",
+                            description: "I can do front end and back end developement in JavaScript " +
+                            "for your website."
+                        },
+                        {
+                            UserId: userId,
+                            title: "Full Stack Development",
+                            description: "Give me your idea and I will take you from start to finish " +
+                            "to develop your app. This means front end design to backend logic."
+                        }
+                    ])
+                })
+                .then(function (userData) {
+
+                    res.json("It worked");
+                })
 
         });
 
